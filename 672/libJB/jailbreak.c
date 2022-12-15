3f665de2b1a76ed4098d32e21c7de4658a960db3
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2022-12-15 10:24:12 +0100

    Update jailbreak.c

--- 672/libJB/jailbreak.c
+++ 672/libJB/jailbreak.c
@@ -5,61 +5,75 @@
 static uintptr_t prison0;
 static uintptr_t rootvnode;
 
-static int resolve(void)
-{
-restart:;
-    uintptr_t td = jbc_krw_get_td();
-    uintptr_t proc = jbc_krw_read64(td+8, KERNEL_HEAP);
-    for(;;)
-    {
-        int pid;
-        if(jbc_krw_memcpy((uintptr_t)&pid, proc+0xb0, sizeof(pid), KERNEL_HEAP))
+static int resolve(void) {
+    // Declare local variables
+    uintptr_t td, proc, pid1_ucred, pid1_fd;
+    int pid;
+
+    // Get the thread descriptor for the current thread
+    td = jbc_krw_get_td();
+    // Read the proc structure associated with the current thread
+    proc = jbc_krw_read64(td + 8, KERNEL_HEAP);
+
+    // Loop until we find the process with PID 1 (init)
+    for (;;) {
+        // Read the PID of the current process
+        if (jbc_krw_memcpy((uintptr_t)&pid, proc + 0xb0, sizeof(pid), KERNEL_HEAP)) {
+            // If the memory copy fails, start over
             goto restart;
-        if(pid == 1)
-            break;
+        }
+
+        // If the PID is 1, we have found the init process
+        if (pid == 1){break;}
+
+        // Read the proc2 and proc1 structures associated with the current process
         uintptr_t proc2 = jbc_krw_read64(proc, KERNEL_HEAP);
-        uintptr_t proc1 = jbc_krw_read64(proc2+8, KERNEL_HEAP);
-        if(proc1 != proc)
-            goto restart;
+        uintptr_t proc1 = jbc_krw_read64(proc2 + 8, KERNEL_HEAP);
+
+        // If proc1 and proc2 are not the same, start over
+        if (proc1 != proc){goto restart;}
+
+        // Update the proc variable for the next iteration of the loop
         proc = proc2;
     }
-    uintptr_t pid1_ucred = jbc_krw_read64(proc+0x40, KERNEL_HEAP);
-    uintptr_t pid1_fd = jbc_krw_read64(proc+0x48, KERNEL_HEAP);
-    if(jbc_krw_memcpy((uintptr_t)&prison0, pid1_ucred+0x30, sizeof(prison0), KERNEL_HEAP))
+
+    // Read the pid1_ucred and pid1_fd structures associated with the init process
+    pid1_ucred = jbc_krw_read64(proc + 0x40, KERNEL_HEAP);
+    pid1_fd = jbc_krw_read64(proc + 0x48, KERNEL_HEAP);
+
+    // Copy the prison0 and rootvnode values from these structures into local variables
+    if (jbc_krw_memcpy((uintptr_t)&prison0, pid1_ucred + 0x30, sizeof(prison0), KERNEL_HEAP)){
+        // If the memory copy fails, return -1 to indicate an error
         return -1;
-    if(jbc_krw_memcpy((uintptr_t)&rootvnode, pid1_fd+0x18, sizeof(rootvnode), KERNEL_HEAP))
-    {
+    }
+    if (jbc_krw_memcpy((uintptr_t)&rootvnode, pid1_fd + 0x18, sizeof(rootvnode), KERNEL_HEAP)){
+        // If the memory copy fails, set prison0 to 0 and return -1 to indicate an error
         prison0 = 0;
         return -1;
     }
+
     return 0;
 }
 
-uintptr_t jbc_get_prison0(void)
-{
-    if(!prison0)
-        resolve();
+
+uintptr_t jbc_get_prison0(void) {
+    if(!prison0) resolve();
     return prison0;
 }
 
-uintptr_t jbc_get_rootvnode(void)
-{
-    if(!rootvnode)
-        resolve();
+uintptr_t jbc_get_rootvnode(void) {
+    if(!rootvnode) resolve();
     return rootvnode;
 }
 
-static inline int ppcopyout(void* u1, void* u2, uintptr_t k)
-{
+static inline int ppcopyout(void* u1, void* u2, uintptr_t k) {
     return jbc_krw_memcpy((uintptr_t)u1, k, (uintptr_t)u2-(uintptr_t)u1, KERNEL_HEAP);
 }
 
-static inline int ppcopyin(const void* u1, const void* u2, uintptr_t k)
-{
+static inline int ppcopyin(const void* u1, const void* u2, uintptr_t k) {
     return jbc_krw_memcpy(k, (uintptr_t)u1, (uintptr_t)u2-(uintptr_t)u1, KERNEL_HEAP);
 }
-int jbc_get_cred(struct jbc_cred* ans)
-{
+int jbc_get_cred(struct jbc_cred* ans) {
     uintptr_t td = jbc_krw_get_td();
     uintptr_t proc = jbc_krw_read64(td + 8, KERNEL_HEAP);
     uintptr_t ucred = jbc_krw_read64(proc + 0x40, KERNEL_HEAP);
@@ -75,8 +89,7 @@ int jbc_get_cred(struct jbc_cred* ans)
     return 0;
 }
 
-int jbc_set_cred(const struct jbc_cred* ans)
-{
+int jbc_set_cred(const struct jbc_cred* ans) {
     uintptr_t td = jbc_krw_get_td();
     uintptr_t proc = jbc_krw_read64(td + 8, KERNEL_HEAP);
     uintptr_t ucred = jbc_krw_read64(proc + 0x40, KERNEL_HEAP);
@@ -92,14 +105,11 @@ int jbc_set_cred(const struct jbc_cred* ans)
     return 0;
 }
 
-int jbc_jailbreak_cred(struct jbc_cred* ans)
-{
+int jbc_jailbreak_cred(struct jbc_cred* ans) {
     uintptr_t prison0 = jbc_get_prison0();
-    if (!prison0)
-        return -1;
+    if (!prison0) return -1;
     uintptr_t rootvnode = jbc_get_rootvnode();
-    if (!rootvnode)
-        return -1;
+    if (!rootvnode) return -1;
 
     //without some modules wont load like Apputils
     ans->sceProcCap = 0xffffffffffffffff;
