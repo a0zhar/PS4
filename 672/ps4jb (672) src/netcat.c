bcec5e10c510e22a365cd1a7c8fb6b7aa02e2399
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2022-12-16 15:46:17 +0100

    Update netcat.c

--- 672/ps4jb (672) src/netcat.c
+++ 672/ps4jb (672) src/netcat.c
@@ -12,34 +12,53 @@
 
 #define MIRA_BLOB_1_LEN "$(window.mira_blob_len||0)"
 #define MIRA_BLOB_2_LEN "$(window.mira_blob_2_len||0)"
-
 void *sender_thread(void *_) {
-	char *mira_blob_2 = __builtin_gadget_addr(MIRA_BLOB_2);
+	// mira_blob_2 is a pointer to the address of MIRA_BLOB_2
+	char *mira_blob_2_ptr = __builtin_gadget_addr(MIRA_BLOB_2);
+	// mira_blob_2_len is an integer containing the address of MIRA_BLOB_2_LEN
 	int mira_blob_2_len = __builtin_gadget_addr(MIRA_BLOB_2_LEN);
-	if (!mira_blob_2) return NULL;
+	// If mira_blob_2_ptr is NULL (0), return NULL
+	if (!mira_blob_2_ptr) return NULL;
 
+	// Sleep for nanoseconds specified in the string "\2\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
 	nanosleep("\2\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", NULL);
-	int q = socket(AF_INET, SOCK_STREAM, 0);
+	// Create a new socket with AF_INET (IPv4), SOCK_STREAM (TCP), and default protocol
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	// Set up a sockaddr_in structure with the following values:
+	// - sin_family: AF_INET (IPv4)
+	// - sin_addr: {.s_addr = 0x100007f} (inet_aton("127.0.0.1"))
+	// - sin_port: 0x3d23 (htons(9021))
 	struct sockaddr_in addr = {
 		.sin_family = AF_INET,
-		.sin_addr = {.s_addr = 0x100007f}, // inet_aton(127.0.0.1)
-		.sin_port = 0x3d23, // htons(9021)
+		.sin_addr = {.s_addr = 0x100007f},
+		.sin_port = 0x3d23,
 	};
-	connect(q, &addr, sizeof(addr));
-	char *mirabuf = mira_blob_2;
+	// Connect to the socket specified by addr
+	connect(socket_fd, &addr, sizeof(addr));
+	// Set mirabuf to the address of mira_blob_2
+	char *mirabuf = mira_blob_2_ptr;
+	// Set lenleft to the value of mira_blob_2_len
 	int lenleft = mira_blob_2_len;
 
-	// While lenleft is greater than 0 aka FALSE
+	// While lenleft is greater than 0
 	while (lenleft) {
-		int chunk = write(q, mirabuf, lenleft);
-		if (chunk <= 0) // if written is less than or equal to 0
-			break;// exit  loop
+		// Write the contents of mirabuf to the socket specified by socket_fd
+		// chunk will be the number of bytes written
+		int chunk = write(socket_fd, mirabuf, lenleft);
+		// If chunk is less than or equal to 0, exit the loop
+		if (chunk <= 0)
+			break;
+		// Move mirabuf to the next chunk of data
 		mirabuf += chunk;
+		// Decrement lenleft by the number of bytes written
 		lenleft -= chunk;
 	}
-	close(q);
+	// Close the socket
+	close(socket_fd);
+	// Return NULL
 	return NULL;
 }
+
 
 int main() {
 	if (setuid(0))
